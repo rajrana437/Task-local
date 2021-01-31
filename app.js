d94decd6f6772be9017ff40c45f02e549c4b5de4
Raj <rajrana437@gmail.com>
2021-01-31 19:29:10 +0530

    login and register post request added

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require('mongoose');
+const app = express();
+
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+
 mongoose.connect("mongodb://localhost:27017/myblogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
 const homeStartingContent = "The secret of change is to focus all of your energy, not on fighting the old, but building on the new.";
 const author = "Socrates, Philosopher";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
+
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const postSchema = new mongoose.Schema({
   title: {
@@ -27,11 +40,33 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+}); //post schema
+
+
+const Post = mongoose.model("Post", postSchema); //posts model
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+}); //User details schema
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
 
-const Post = mongoose.model("Post", postSchema);
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
-//const posts = [];
 
 app.get("/", function(req, res){
   res.render("welcome")
@@ -68,14 +103,7 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  // const post = {
-  //   title: req.body.postTitle,
-  //   content: req.body.postBody
-  // };
-  //
-  // posts.push(post);
-  //
-  // res.redirect("/");
+
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
@@ -89,21 +117,45 @@ app.post("/compose", function(req, res){
   res.redirect("/");
 });
 
-// app.get("/posts/:postId", function(req, res){
-//   const requestedTitle = _.lowerCase(req.params.postName);
-//
-//   posts.forEach(function(post){
-//     const storedTitle = _.lowerCase(post.title);
-//
-//     if (storedTitle === requestedTitle) {
-//       res.render("post", {
-//         title: post.title,
-//         content: post.content
-//       });
-//     }
-//   });
-//
-// });
+
+
+
+
+app.post("/register", function(req, res){
+
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      })
+    }
+  })
+
+});
+
+app.post("/login", function(req, res){
+
+const user =new User({
+  username:req.body.username,
+  password:req.body.password
+});
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      });
+    }
+  });
+});
+
+
+
+
+
 const postId = Post._id;
 app.get("/posts/:postId", function(req, res){
   const requiredPostId = req.params.postId;
